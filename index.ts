3b8f991307fbe6f8845395c14a77583e12469662
Albaloshi9 <userdied722@gmail.com>
2024-03-23 05:56:53 -0700

    simple mistakes changes

--- index.ts
+++ index.ts
@@ -3,29 +3,28 @@ import inquirer from "inquirer";
 
 //computer generated number
 // const guessingNumber = Math.floor(Math.random() * 10 + 1);
-let guessingNumber = 5;
 let numTries = 10;
 
 //user input number 
 while(numTries>0){
-let userNumber = await inquirer.prompt([{
-    name: "guessingNumber",
+const guessingNumber = Math.floor(Math.random() * 10 + 1);
+let userInput = await inquirer.prompt([{
+    name: "userNumber",
     type: "number",
     message: "guess the number between 1-10 >"
 }]);
 
 
-if(guessingNumber === userNumber.guessingNumber){
+if(userInput.userNumber === guessingNumber){
     console.log("You Guessed Right ,\nGame Over")
     numTries = 0;
 }else{
     console.log(`You Guessed it wrong the correct number is : ${guessingNumber}`);
-    if(guessingNumber>userNumber.guessingNumber){
+    if(guessingNumber>userInput.userNumber){
         console.log("Think Higher");
     }else{
         console.log("Think Lower");
-        
-    }
+         }
     console.log(`You have ${numTries -1} left`);
 }
 numTries--;
